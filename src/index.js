f776c03661cd73cb6f7c781176d66f1737665d76
Nate Tan <nktan93@gmail.com>
2019-12-06 21:48:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'; /* code change */
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer); /* code change */
+const store = createStore(shoppingListItemReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// code change - added Provider to wrap around App
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider> /* code change */,
+  document.getElementById('root')
+);
